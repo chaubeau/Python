6c8e6657093a1bb3b710a363bfc7045654a75e09
chaubeau <chaubeau01@gmail.com>
2014-03-12 16:49:17 +0800

    study python 2day

---
+++ 2day/cal.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+#  Date: 2014-03-12 09:34:27
+#
+#  Copyright 2014 chaubeau <chaubeau01@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  Desc :
+#
+
+number1 = int(raw_input("Please input number1: "))
+number2 = int(raw_input("Please input number2: "))
+
+print "number1 + number2 = %d\n" %(number1 + number1)
+
+print "number1 - number2 = %d\n" %(number1 - number2)
+
+print "number1 * number2 = %d\n" %(number1 * number2)
+
+print "number1 / number2 = %d\n" %(number1 / number2)
+
+print "number < number2: %s\n" %(number1 < number2)
+
+print "number1 > number2: %s\n" %(number1 > number2)
+
+print "number1 == number2: %s\n" %(number1 == number2)
+
+print "number1 << number2 = %d\n" %(number1 << number2)
+
+print "number1 >> number2= %d\n" %(number1 >> number1)
+
+print "number1 mod number2 = %d" %(number1 % number2)
