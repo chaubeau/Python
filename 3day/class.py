3803b53f17dcfa1b6084dc37481d14b40e4682a8
chaubeau <chaubeau01@gmail.com>
2014-03-24 15:37:54 +0800

    study python 3day

---
+++ 3day/class.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+#  Date: 2014-03-20 15:30:21
+#
+#  Copyright 2014 chaubeau <chaubeau01@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  Desc :
+#
+str1=raw_input()
+print str1
+
+print str1.lower()
+print str1.islower()
+
+print str1.upper()
+#help(str)
+#print str1.statwith('f')
+
+#print str1.endwith('l')
---
+++ 3day/import_tmp.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+#  Date: 2014-03-20 15:16:57
+#
+#  Copyright 2014 chaubeau <chaubeau01@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  Desc :
+#
+
+import convert
+
+print convert.to_cel(33.3)
+
+print convert.above_fr(22.0)
+
+print "ehco:__name__ is",__name__
+
+import echo
+help(convert)
+
+if __name__ == '__name__':
+    help(name)
+
