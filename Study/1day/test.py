cbaa21f260c535964d2438b7015bc3acdf1c02c3
chaubeau <chaubeau01@gmail.com>
2014-07-31 15:53:15 +0800

    update

--- Study/1day/test.py
+++ Study/1day/test.py
@@ -2,33 +2,29 @@
 # -*- coding: utf-8 -*-
 #
 #  test.py
-#  
+#
 #  Copyright 2014 bo <chaubeau@gmail.com>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
-
-
+#
 
 def main():
-	val=1024;
-	print id(val);
-	print id(val)+1024;
-	
+	val=1024
+	print id(val)
+	print id(val)+1024
 	return 0
 
 if __name__ == '__main__':
